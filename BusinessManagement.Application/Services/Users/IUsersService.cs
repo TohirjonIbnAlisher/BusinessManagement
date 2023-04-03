3892435b18bc2afdc0ad3c9c69d2e87049d28d01
Jasurbek <mamatqulovjasurbek805@gmail.com>
2023-04-03 18:20:05 +0500

    Added user service and User DTO to Service Layer

---
+++ BusinessManagement.Application/Services/Users/IUsersService.cs
@@ -0,0 +1,12 @@
+﻿using BusinessManagement.Application.DataTransferObjects;
+
+namespace BusinessManagement.Application.Services;
+
+public interface IUsersService
+{
+    ValueTask<UserDTO> CreateUserAsync(UserCreationDTO userCreationDTO);
+    IQueryable<UserDTO> RetrieveUsers();
+    ValueTask<UserDTO> RetrieveUserByIdAsync(Guid userId);
+    ValueTask<UserDTO> ModifyUserAsync(ModifyUserDTO modifyUserDTO);
+    ValueTask<UserDTO> RemoveUserAsync(Guid userId);
+}
---
+++ BusinessManagement.Application/Services/Users/UserService.cs
@@ -0,0 +1,31 @@
+﻿using BusinessManagement.Application.DataTransferObjects;
+
+namespace BusinessManagement.Application.Services;
+
+public class UserService : IUsersService
+{
+    public ValueTask<UserDTO> CreateUserAsync(UserCreationDTO userCreationDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<UserDTO> ModifyUserAsync(ModifyUserDTO modifyUserDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<UserDTO> RemoveUserAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<UserDTO> RetrieveUserByIdAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<UserDTO> RetrieveUsers()
+    {
+        throw new NotImplementedException();
+    }
+}
